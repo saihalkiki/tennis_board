48331867328358f97ae763850a92d939a3966edb
Haruka <saito75061@gmail.com>
2019-06-13 23:00:35 +0900

    added user/models

--- db/migrate/20190612113845_create_users.rb
+++ db/migrate/20190612113845_create_users.rb
@@ -6,5 +6,6 @@ class CreateUsers < ActiveRecord::Migration[5.2]
 
       t.timestamps
     end
+    add_index :users, :email, unique: true
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "User_model 空白エラーテスト" do
+    @user.name = " "
+    @user.email = " "
+    assert_not @user.valid?
+  end
 end
