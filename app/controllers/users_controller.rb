4631113bafe95474213ef67a2fad8a02b909c691
Haruka <saito75061@gmail.com>
2019-06-16 18:36:16 +0900

    added error_massage and modify user_create_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,15 +26,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
+    # respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        # format.html { redirect_to @user, notice: 'User was successfully created.' }
+        # format.json { render :show, status: :created, location: @user }
+        flash[:success] = "登録が完了しました！"
+        redirect_to @user
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        # format.html { render :new }
+        # format.json { render json: @user.errors, status: :unprocessable_entity }
+        render 'new'
       end
-    end
+    # end
   end
 
   # PATCH/PUT /users/1
@@ -67,8 +70,8 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    # Strong Parameters
     def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
