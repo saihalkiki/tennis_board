738bc5c6ff5c2fd77ccaed5637faa47c1ca13362
Haruka <saito75061@gmail.com>
2019-06-16 09:07:28 +0900

    debug custom

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,length: {maximum: 255},
   format: { with: VALID_EMAIL_REGEX },
-  uniqueness: { case_sensitive: false }  # メールアドレスの大文字小文字を無視した一意性
+  uniqueness: { case_sensitive: false }
+  # メールアドレスの大文字小文字を無視した一意性
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}
 end
