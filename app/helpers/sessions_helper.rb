cbf4734bd71c684d11f1e527044c746186d91664
Haruka <saito75061@gmail.com>
2019-06-17 11:25:00 +0900

    add method log_in(user) adn current_user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,14 @@
 module SessionsHelper
+  # 渡されたユーザーでログインするヘルパー
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # 現在ログイン中のユーザーデータ(いない場合はnilを返す)
+  def current_user
+    if !!session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+      # session[:user_id] = user.id
+  end
+
 end
