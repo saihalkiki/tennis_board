25abfe394ff30e6dd56ca935918e8bcc76d7bc7b
Haruka <saito75061@gmail.com>
2019-06-17 10:04:32 +0900

    login_form

--- db/migrate/20190612113845_create_users.rb
+++ db/migrate/20190612113845_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :name # , null: false →NOT NULL制約
       t.string :email
 
       t.timestamps
