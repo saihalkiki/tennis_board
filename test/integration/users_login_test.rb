530f8dcd365fc02229f1ffa0c1d6ae8658760a55
Haruka <saito75061@gmail.com>
2019-06-17 19:34:05 +0900

    add logout system

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -16,15 +16,21 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.blank?
   end
 
-  test "ログイン成功時のテスト" do
+  test "ログイン成功/ログアウトのテスト" do
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { email: @user.email, password: 'password' } }
+    assert logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
-    assert logged_in?
+    delete logout_path
+    assert_not logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
   end
 end
