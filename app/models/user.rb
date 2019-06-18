b33fb61b4315b7208b4f8873574108d482094f93
Haruka <saito75061@gmail.com>
2019-06-18 10:17:21 +0900

    Added methods of random_token, remember_token, and remember_token authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name, presence: true,length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -13,6 +14,24 @@ class User < ApplicationRecord
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :  BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember_token
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # 渡されたトークンがダイジェストと一致したらtrue
+  def authenticated?(remember_token)
+    BCrypt::Password.new(attribute_digest).is_password?(remember_token)
+    # secure_passwordのソースコード：https://github.com/rails/rails/blob/master/activemodel/lib/active_model/secure_password.rbを参照
+    # .is_password？メソッドは==と同等。bcrypt gemのソースコードのソースコードを参照：https://github.com/codahale/bcrypt-ruby/blob/master/lib/bcrypt/password.rb
   end
 
 end
